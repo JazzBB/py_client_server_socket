12a05ea23b2e37817302ed32a50936f30927d20f
JazzBB <30501653+JazzBB@users.noreply.github.com>
2019-01-29 16:30:10 +0100

    corrected while loop

--- server/server.py
+++ server/server.py
@@ -8,23 +8,22 @@ port = 12345
 socket_server.bind((host, port))
 socket_server.listen(5)
 
-client, addr = socket_server.accept()
-client.send('Connection accepted at '+ host)
-print('Received connection from', addr)
+print("#######################")
+print("started server side")
+print("#######################")
+
+no_data = 0
 
 def listener():
-    while True:
         data = client.recv(4096).decode()
         print('Command received from -'+ str(addr) +' - ' + data)
 
-        if not data:
-            break
 
         if data == 'srv_uptime':
             return_payload = os.popen('uptime').read()
             client.send(str(return_payload))
             print('Response was :' + return_payload)
-            return listener()     
+            return listener()
    
         if data == 'srv_shut':
             return_payload = 'Linux shut down initiated'
@@ -33,10 +32,15 @@ def listener():
             return listener()
 
         if data == 'close':
-            return_payload = 'Client-side request has been received\nClosing conn ection'
+            return_payload = 'Client-side request has been received\nClosing connection'
             print(return_payload)
-            return listener()
+            client.close()
+            #no_data = 1
+            #eturn no_data
 
-if __name__ == '__main__':
+
+while no_data == 0:
+    client, addr = socket_server.accept()
+    client.send('Connection : '+ host)
+    print('Accepted connection request from: ' + str(addr))
     listener()
-
